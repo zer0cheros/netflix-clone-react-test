ceba5fe3d372bc6079caf69b73c946e206a640c9
Zer0cheros <christian.wiss@optimaedu.fi>
2022-02-25 14:31:56 +0200

    update

--- src/componet/Content.js
+++ src/componet/Content.js
@@ -3,12 +3,14 @@ import React from 'react'
 function Content() {
   return (
     <div className='content'>
+      <div className='text'>
         <h1>Obegränsat med filmer, serier och annat.</h1>
         <h2>Titta var du vill. Avsluta när du vill.</h2>
         <h3>Redo att börja titta? Ange din e-postadress för att skapa eller återaktivera ett konto.</h3>
+      </div>
         <form className='form-control'>
-            <input className='input'/>
-            <button className='try'>Kom igång</button>
+            <input className='input' placeholder='E-postadress'/>
+            <button className='try'>Kom igång </button>
         </form>
     </div>
   )
