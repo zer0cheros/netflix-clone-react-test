422aa9df07992bea72b0f530372309d1a95672e3
Zer0cheros <christian.wiss@optimaedu.fi>
2022-03-15 14:28:11 +0200

    test

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Browser from '../componet/Browser'
+import Logo from '../componet/Logo'
 
 function Browse() {
   return (
     <div className='container'>
         <div className='browse-container'>
+            <Logo/>
             <Browser/>
         </div>    
     </div>
